9308c7e679fcb020aa403ba20470d6d9307a3601
Robert Lail <robert.lail@cph.org>
2012-09-18 13:53:58 -0500

    initial commit of Rails application for developing and testing pdfjs-rails gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PdfjsRailsDev::Application.config.secret_token = 'b201c465f9da3c22b2406f1db639f27940da5cab377577e3beae7023ff3f11bafd44e7b63b077452c1b3f9cd0391613a366430101cdb22be6532ae66117b2d17'
